c1acb2a40c7efcad9ede07350e801bbde5183ca8
Gabriel <bielmgama@gmail.com>
2024-05-14 02:46:47 -0300

    added day 1 part 1

--- 2023/python/day 01/part_1.py
+++ 2023/python/day 01/part_1.py
@@ -1,2 +1,21 @@
 lines = open('input.txt', 'r').readlines()
+result = 0
+number = ''
 
+for line in lines:
+    for i in range(0, len(line)):
+        char = line[i]
+        if char.isnumeric():
+            number += char
+            break
+
+    for i in range(len(line) - 1, -1, -1):
+        char = line[i]
+        if char.isnumeric():
+            number += char
+            break
+
+    result += int(number)
+    number = ''
+
+print(result)
